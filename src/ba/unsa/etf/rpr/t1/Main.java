e6ddfd86e98016bb81966adb9b249b65816968c1
zlxxo <mramic2@etf.unsa.ba>
2019-10-12 19:29:34 +0200

    gotovo

--- src/ba/unsa/etf/rpr/t1/Main.java
+++ src/ba/unsa/etf/rpr/t1/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    private int sumaCifara(int n) {
+    private static int sumaCifara(int n) {
         if(n < 0)  n *= -1;
-        int suma = n % 10;
+        int suma = 0;
         while(n != 0) {
             suma += n % 10;
             n /= 10;
@@ -18,9 +18,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Unesite prirodan broj n: ");
         int n = scanner.nextInt();
+
         while(n < 1) {
             System.out.print("Unesite prirodan broj n: ");
             n = scanner.nextInt();
+        }
+
+        System.out.println("Brojevi od 1 do " + n + " djeljivi sa sumom svojih cifara su:");
+
+        for(int i = 1; i <= n; i++) {
+            if(i % sumaCifara(i) == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
